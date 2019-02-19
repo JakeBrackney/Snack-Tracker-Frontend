943d478ec9be80f9c57f79ad7bd2843aac9c3cfb
“MARIA <MDFORTINO@GMAIL.COM>
2019-02-19 14:56:14 -0500

    install axios

--- src/components/ShowRestaurant/ShowRestaurant.js
+++ src/components/ShowRestaurant/ShowRestaurant.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
 
-class Restaurant extends Component {
+const restaurantsURL = 'http://localhost:3001/api/restaurants'
+
+class ShowRestaurant extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      restaurant: []
+    }
+   
+  }
+  componentDidMount () {
+
+    const restaurantId = this.props.match.params.id
+    console.log(`${restaurantsURL}${restaurantId}`)
+    const url = `${restaurantsURL}${restaurantId}`
+
+    console.log("did mount in showpage")
+    axios.get(url)
+      .then((res) => {
+        console.log(res)
+        this.setState({
+          restaurant: res.data
+        })
+        console.log(this.state.restaurant)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +41,4 @@ class Restaurant extends Component {
   }
 }
 
-export default Restaurant;
+export default ShowRestaurant;
