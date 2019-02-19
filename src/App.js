41f48b581f697a919f9d43c2f3af46dc6150700b
Jake Brackney <brackney.jake@gmail.com>
2019-02-19 12:15:02 -0500

    Updates to App.js, Restaurant.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom'
+import Restaurant from '../src/Restaurant/Restaurant'
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state= {
+      restaurants: []
+    }
+  }
+
   render() {
+
+
+  // let restaurantList = this.props.data.map(stock => {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Snack Tracker
         </header>
+        <div className ='restaurantList'>
+            {/* <Link to={"/restaurants/" + restaurant.name}>
+              <p>{restaurant.name}</p>
+            </Link> */}
+        </div>
+        <div className="restaurant-list">
+          {/* {restaurantList} */}
+        </div>
       </div>
-    );
+    )
   }
 }
 
